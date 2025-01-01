3c7ed674290606826008c52085d433d4f35be648
xiaozhenano <xiaozhedesu0219@outlook.com>
2025-01-01 23:11:58 +0800

    初版，只实现了随机功能，没有做错误处理之类的预防。

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 [![npm](https://img.shields.io/npm/v/koishi-plugin-random-ba-manga?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-random-ba-manga)
 
 在ba官网随机一张ba漫画发出
+
+需要确保bot所在的网络环境能够访问[ba官网](https://bluearchive.jp/),否则可能无法正常工作。
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,39 @@ import { Context, Schema } from 'koishi'
 
 export const name = 'random-ba-manga'
 
-export interface Config {}
+export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({})
 
+/**
+ * 对一个数组随机选取一个下标返回
+ * @param length 数组长度
+ * @returns 整型
+ */
+const random = length => Math.floor(Math.random() * length)
+
 export function apply(ctx: Context) {
-  // write your plugin here
+  ctx.command('random-ba-yonkoma').alias('随个ba四格漫')
+    .action(async ({ session }) => {
+      session.send('请求中，请稍等...');
+      const url = 'https://bluearchive.jp/cms/comic/list?pageIndex=1&pageNum=250&type=1'
+      const response = await fetch(url).then(response => response.json());
+      const data = response.data;
+
+      const index = random(data.comicList.length);
+      const imageUrl = data.comicList[index].comic;
+      session.send(`<img src="${imageUrl}">`);
+    })
+
+  ctx.command('random-ba-aoharuRecord').alias('随个ba青春记录')
+    .action(async ({ session }) => {
+      session.send('请求中，请稍等...');
+      const url = 'https://bluearchive.jp/cms/comic/list?pageIndex=1&pageNum=150&type=2';
+      const response = await fetch(url).then(response => response.json());
+      const data = response.data;
+
+      const index = random(data.comicList.length);
+      const imageUrl = data.comicList[index].comic;
+      session.send(`<img src="${imageUrl}">`);
+    })
 }
